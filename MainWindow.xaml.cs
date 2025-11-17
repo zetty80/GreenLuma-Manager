ad894e486ee08c525df238807f602bfea4f5b848
3vil3vo <julianzon000@gmail.com>
2025-11-17 20:55:56 +0100

    Release RC2.4

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace GreenLuma_Manager;
 
 public partial class MainWindow
 {
-    public const string Version = "RC2.3";
+    public const string Version = "RC2.4";
 
     private readonly ObservableCollection<Game> _games;
     private readonly ObservableCollection<string> _profiles;
